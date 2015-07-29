c46796ed6054557aa469069d67e65ae0c7bac31e
liangfei <njliangfei@gmail.com>
2015-07-29 14:28:03 +0800

    play map-reduce in OC

---
+++ UICollectionViewWithMenu/Item.m
@@ -0,0 +1,13 @@
+//
+//  Item.m
+//  UICollectionViewWithMenu
+//
+//  Created by liangfei on 7/28/15.
+//  Copyright (c) 2015 liangfeizc. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
+@end
--- UICollectionViewWithMenu/ViewController.m
+++ UICollectionViewWithMenu/ViewController.m
@@ -7,33 +7,91 @@
 //
 
 #import "ViewController.h"
+#import "Item.h"
 
 #define NUMBER_OF_CELLS_PER_ROW 6
 #define CELL_WIDTH 166
-#define CELL_HEIGHT = 223
+#define CELL_HEIGHT 223
 
 @interface ViewController ()
-
 @property (nonatomic, copy) NSMutableArray *dataArray;
-
 @property (nonatomic, assign) NSInteger sectionOfMenuCell;
 @property (nonatomic, assign) NSInteger rowOfMenuCell;
-
 @end
 
 @implementation ViewController
 
+- (instancetype)init {
+    if (self = [super init]) {
+        NSMutableArray *array = [NSMutableArray array];
+        
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss zzz"];
+        [dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"]];
+        NSDate *capturedStartDate = [dateFormatter dateFromString: @"2012-09-16 23:59:59 JST"];
+        
+        NSMutableArray *dates = [NSMutableArray new];
+        
+        for (int i = 0; i < 10; i++) {
+            Item *item = [Item new];
+            item.date = [self generateRandomDate];
+            [dates addObject:item.date];
+            item.name = [NSString stringWithFormat:@"item %d", i];
+            [array addObject:item];
+        }
+        
+        NSArray *sortedDates = [dates sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            return [obj2 compare:obj1];
+        }];
+        
+        NSLog(@"%@", sortedDates);
+        NSLog(@"%@", [dates sortedArrayUsingSelector:@selector(compare:)]);
+        
+        return self;
+        
+        NSMutableDictionary *result = [NSMutableDictionary new];
+        NSArray *distinctDates;
+        distinctDates = [array valueForKeyPath:@"@distinctUnionOfObjects.date"];
+        for (NSDate *date in distinctDates) {
+            NSPredicate *predicate = [NSPredicate predicateWithFormat:@"date = %@", date];
+            NSArray *items = [array filteredArrayUsingPredicate:predicate];
+            [result setObject:items forKey:date];
+        }
+        
+        NSLog(@"%@", result);
+    }
+    
+    return self;
+}
+
+- (NSDate *)generateRandomDate {
+    NSDate *today = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSCalendarUnit unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit;
+    NSDateComponents *dateComponents = [calendar components:unitFlags fromDate:today];
+    NSRange days = [calendar rangeOfUnit:NSDayCalendarUnit
+                                  inUnit:NSMonthCalendarUnit
+                                 forDate:today];
+    int r = arc4random() % days.length;
+    [dateComponents setDay:r];
+    NSDate *startDate = [calendar dateFromComponents:dateComponents];
+    
+    return startDate;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self init];
     // create parent view
     // self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
+    return;
     // create an UICollectionView
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
-    layout.sectionInset = UIEdgeInsetsMake(4, 4, 0, 0);
-    layout.minimumInteritemSpacing = 4.f;
-    layout.minimumLineSpacing = 4.f;
+    //layout.sectionInset = UIEdgeInsetsMake(4, 4, 0, 0);
+    //layout.minimumInteritemSpacing = 4.f;
+    //layout.minimumLineSpacing = 4.f;
     self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
     
     [self.collectionView setDataSource:self];
@@ -116,7 +174,7 @@
     
     if ([self isMenuCellAtIndexPath:indexPath]) {
         if ([self willMenuCellShowAtIndexPath:indexPath]) {
-            return CGSizeMake(self.view.frame.size.width, 247);
+            return CGSizeMake(self.view.frame.size.width - 4, 247);
         } else {
             return CGSizeZero;
         }
@@ -173,6 +231,21 @@
 
 - (BOOL)willMenuCellShowAtIndexPath:(NSIndexPath *)indexPath {
     return _sectionOfMenuCell == indexPath.section && _rowOfMenuCell == indexPath.row;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 4.0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 4.0;
+}
+
+// Layout: Set Edges
+- (UIEdgeInsets)collectionView:
+(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    // return UIEdgeInsetsMake(0,8,0,8);  // top, left, bottom, right
+    return UIEdgeInsetsMake(0, 0, 0, 0);  // top, left, bottom, right
 }
 
 @end
